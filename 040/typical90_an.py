6d51a9727f748fa341c849bdbe8f879c3946f8d4
hiro.suzuki <hiro.suzuki.oct10@gmail.com>
2021-12-19 13:21:06 +0000

    競プロ典型 90 問 040

---
+++ 040/typical90_an.py
@@ -0,0 +1,15 @@
+# https://atcoder.jp/contests/typical90/tasks/typical90_an
+
+from typing import List
+
+#from collections import 
+
+N, W = [int(x) for x in input().split()]
+A = [int(x) for x in input().split()]
+KC = [[int(x) for x in input().split()] for _ in range(N)]
+
+def solve(N: int, W: int, A: List[int], KC: List[List[int]]):
+    ...
+
+
+solve(N, W, A, KC)
